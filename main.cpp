490f07892b9d91bd09fb42bcff54c0b1e36c9fd2
tyllcimun <annamihalna2005@gmail.com>
2025-04-23 19:05:01 +0300

    Написана функция main, которая обрабатывает аргументы командной строки
    При аргументе "-help": реализован вывод справочной информации о программе.
    При вводе двух аргументов: реализована заглушка для получения объяснения выражения.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,61 @@
+#include "expression.h"
+#include "teexception.h"
+
 #include <QCoreApplication>
+#include <QFileInfo>
+#include <windows.h>
+#include <conio.h>
+#include <QTextStream>
+
+
+
+void printHelpMessage(QTextStream& cout, const QString& filename);
+void printExpanssion(QTextStream& cout, const QString& inputFile, const QString& outputFile);
+void printExplanation(QTextStream& cout, const QString& inputFile, const QString& outputFile);
+
 
 int main(int argc, char *argv[])
 {
+    SetConsoleOutputCP(CP_UTF8);
+
     QCoreApplication a(argc, argv);
+    QTextStream cout(stdout);
+    // cout.setEncoding(QStringConverter::Utf8);
 
-    // Set up code that uses the Qt event loop here.
-    // Call a.quit() or a.exit() to quit the application.
-    // A not very useful example would be including
-    // #include <QTimer>
-    // near the top of the file and calling
-    // QTimer::singleShot(5000, &a, &QCoreApplication::quit);
-    // which quits the application after 5 seconds.
+    QString fileName = QCoreApplication::applicationFilePath();
+    QFileInfo fileInfo(fileName);
+    fileName = fileInfo.fileName();
 
-    // If you do not need a running Qt event loop, remove the call
-    // to a.exec() or use the Non-Qt Plain C++ Application template.
+    // Если первый аргумент "-help"
+    if(QString(argv[1]) == "-help") {
+        // Напечатать справочную информацию
+        printHelpMessage(cout, fileName);
+    }
+    // Если аргумента три и второй не начинается с "-"
+    else if(argc == 3 && !QString(argv[2]).startsWith("-")) {
+        // Получить объяснение выражения
+        printExplanation(cout, argv[1], argv[2]);
+    }
+    else {
+        cout << ("Ошибка в синтаксисе команды. Подробнее: .\\" + fileName +  " -help");
+    }
 
-    return a.exec();
+    a.exit(0);
+    return 0;
+}
+
+
+void printExplanation(QTextStream& cout, const QString& inputFile, const QString& outputFile){
+}
+void printHelpMessage(QTextStream& cout, const QString& filename)
+{
+    cout << ".\\" + filename + " [-help | -test] [input-file] [output-file]\n";
+    cout << "-help      - Выводит сообщение-помощник. При вводе этой команды путь к файлам указывать не нужно.\n";
+    cout << "-test      - Запускает тесты. При вводе этой команды путь к файлам указывать не нужно.\n";
+    cout << "input-file - путь к входному файлу. В случае, если в пути файла присутствуют пробелы, необходимо указать путь в кавычках. Например:\n";
+    cout << "               \"C:\\\\input files\\input.txt\"\n";
+    cout << "output-file - путь к выходному файлу. Если файла не существует - он будет создан. В случае, если в пути файла присутствуют пробелы, необходимо указать путь в кавычках. Например:\n";
+    cout << "               \"C:\\\\output files\\output.txt\"\n";
+    cout << "Пример запуска: \n";
+    cout << "   .\\" + filename + " input.txt \"C:\\\\files\\New folder\\output.txt\"\n";
 }
