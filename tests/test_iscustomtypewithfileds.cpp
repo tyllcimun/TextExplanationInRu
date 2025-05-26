d9ba6bb57290d875608cae452fcd38b94906d1d0
tyllcimun <annamihalna2005@gmail.com>
2025-05-26 19:13:02 +0300

    Реализация тестов для функции isCustomTypeWithFields

--- tests/test_iscustomtypewithfileds.cpp
+++ tests/test_iscustomtypewithfileds.cpp
@@ -8,9 +8,66 @@ test_isCustomTypeWithFileds::test_isCustomTypeWithFileds(QObject *parent)
 
 void test_isCustomTypeWithFileds::isCustomTypeWithFileds()
 {
+    QFETCH(QString, string);
+    QFETCH(bool, result);
+    QFETCH(Expression, expression);
+
+    QCOMPARE(expression.isCustomTypeWithFields(string), result);
 }
 
 void test_isCustomTypeWithFileds::isCustomTypeWithFileds_data()
 {
+    QTest::addColumn<QString>("string");
+    QTest::addColumn<Expression>("expression");
+    QTest::addColumn<bool>("result");
 
+    QTest::newRow("empty-data")
+        << "a"
+        << Expression("", {}, {}, {}, {}, {}, {})
+        << false;
+
+    QTest::newRow("empty-name-search-in-filled-data")
+        << ""
+        << Expression("", {}, {}, {{"c", Union("c")}}, {{"b", Structure("b")}}, {{"a", Class("a")}}, {{"d", Enum("d")}})
+        << false;
+
+    QTest::newRow("name-not-in-data-types")
+        << "e"
+        << Expression("", {}, {}, {{"c", Union("c")}}, {{"b", Structure("b")}}, {{"a", Class("a")}}, {{"d", Enum("d")}})
+        << false;
+
+    QTest::newRow("correct-class-name")
+        << "a"
+        << Expression("", {}, {}, {}, {}, {{"a", Class("a")}}, {})
+        << true;
+
+    QTest::newRow("correct-structure-name")
+        << "a"
+        << Expression("", {}, {}, {}, {{"a", Structure("a")}}, {}, {})
+        << true;
+
+    QTest::newRow("correct-union-name")
+        << "a"
+        << Expression("", {}, {}, {{"a", Union("a")}}, {}, {}, {})
+        << true;
+
+    QTest::newRow("correct-enum-name")
+        << "a"
+        << Expression("", {}, {}, {}, {}, {}, {{"a", Enum("a")}})
+        << false;
+
+    QTest::newRow("name-contains-search-string")
+        << "a"
+        << Expression("", {}, {}, {}, {}, {{"abv", Class("abv")}}, {})
+        << false;
+
+    QTest::newRow("case-mismatch-lower-string-upper-type")
+        << "a"
+        << Expression("", {}, {}, {}, {}, {{"A", Class("A")}}, {})
+        << false;
+
+    QTest::newRow("case-mismatch-upper-string-lower-type")
+        << "A"
+        << Expression("", {}, {}, {}, {}, {{"a", Class("a")}}, {})
+        << false;
 }
